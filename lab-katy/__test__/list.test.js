8e25cb3bc63088419ca60276e271241d5747dfc2
Katy Robinson <katyorob@gmail.com>
2018-02-28 17:08:12 -0800

    4 tests passing

--- lab-katy/__test__/list.test.js
+++ lab-katy/__test__/list.test.js
@@ -11,9 +11,9 @@ require('jest');
 const url = `http://localhost:${PORT}`;
 const exampleList = {
   name: 'test list name',
-}
+};
 
-//POST TEST
+//POST TESTS
 
 describe('List Routes', function() {
   describe('POST: /api/list', function() {
@@ -21,8 +21,8 @@ describe('List Routes', function() {
       afterEach( done => {
         if(this.tempList) {
           List.remove({}) //removes EVERYTHING. this is fine for today but  NOT if you're actually working with a filled database
-          .then( () => done())
-          .catch(done);
+            .then( () => done())
+            .catch(done);
           return;
         }
         done();
@@ -30,38 +30,51 @@ describe('List Routes', function() {
 
       it('should return a list', done => {
         request.post(`${url}/api/list`)
-        .send(exampleList)
-        .end((err, res) => {
-          if(err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(res.body.name).toEqual('test list name');
-          this.tempList = res.body;
-          done();
+          .send(exampleList)
+          .end((err, res) => {
+            if(err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(res.body.name).toEqual('test list name');
+            this.tempList = res.body;
+            done();
+          });
+      });
+
+      //POST 400 test not passing
+      describe('with no request body', function() {
+        it('should respond with bad request', done => {
+          request.post(`${url}/api/list`)
+            .send({})
+            .end((err, res) => {
+              // console.log(res.body);
+              expect(res.status).toEqual(400);
+              done();
+            });
         });
       });
     });
   });
 
-//GET TEST
+  //GET TESTS
 
   describe('GET: /api/list/:listId', function() {
     describe('with a valid body', function() {
       beforeEach( done => {
         exampleList.timestamp = new Date();
         new List(exampleList).save()
-        .then( list => {
-          this.tempList = list;
-          done();
-        })
-        .catch(done);
+          .then( list => {
+            this.tempList = list;
+            done();
+          })
+          .catch(done);
       });
 
       afterEach( done => {
         delete exampleList.timestamp;
         if (this.tempList) {
           List.remove({})
-          .then( () => done())
-          .catch(done);
+            .then( () => done())
+            .catch(done);
           return;
         }
         done();
@@ -69,12 +82,66 @@ describe('List Routes', function() {
 
       it('should return a list', done => {
         request.get(`${url}/api/list/${this.tempList._id}`)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(res.body.name).toEqual('test list name');
-          done();
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(res.body.name).toEqual('test list name');
+            done();
+          });
+      });
+
+      //GET 404 test not passing
+      describe('with id not found', function() {
+        it('should return 404', done => {
+          request.get(`${url}/api/list/`)
+            .end((err, res) => {
+              expect(res.status).toEqual(404);
+              done();
+            });
         });
+      });
+    });
+  });
+
+
+  // PUT TESTS
+
+  describe('PUT :/api/list/:listId', function() {
+    describe('with a valid body', function() {
+      beforeEach( done => {
+        exampleList.timestamp = new Date();
+        new List(exampleList).save()
+          .then( list => {
+            this.tempList = list;
+            done();
+          })
+          .catch(done);
+      });
+
+      afterEach( done => {
+        if(this.tempList) {
+          List.remove({})
+            .then( () => done())
+            .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('should return an updated list', done => {
+        let updateList = { name: 'test updated list name' };
+        console.log('TEST UPDATE:', this.tempList._id);
+
+        request.put(`${url}/api/list/${this.tempList._id}`)
+          .send(updateList)
+          .end((err, res) => {
+            if(err) return done(err);
+            expect(res.status).toEqual(200);
+            console.log('RES BODY', res.body);
+            expect(res.body._id).toEqual(this.tempList._id.toString());
+            expect(res.body.name).toEqual(updateList.name);
+            done();
+          });
       });
     });
   });
--- lab-katy/route/list-route.js
+++ lab-katy/route/list-route.js
@@ -4,8 +4,10 @@ const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 const debug = require('debug')('weed:list-route');
 const List = require('../model/list.js');
+const createError = require('http-errors');
 
 const listRouter = module.exports = new Router();
+
 
 //GET
 
@@ -14,19 +16,48 @@ listRouter.get('/api/list/:listId', function(req, res, next) {
 
   // mongoose method - go to our list collection with this id, grab that list and return it to me
   List.findById(req.params.listId)
-  .then( list => res.json(list))
-  .catch(next);
+    .then( list => res.json(list))
+    .catch( err => next(err));
 });
+
+listRouter.get('/api/list', function(req, res) {
+  debug('GET: /api/list');
+
+  res.status(404).send('not found');
+});
+
 
 //POST
 
 listRouter.post('/api/list', jsonParser, function(req, res, next) {
   debug('POST: /api/list');
+  
+  if(req.body === {})
+    res.status(400).send('bad request');
 
   req.body.timestamp = new Date();
   new List(req.body).save()
-  .then( list => res.json(list))
-  .catch(next);
+    .then( list => res.json(list))
+    .catch( err => next(err));
 });
 
+//PUT //check up on this
+
+listRouter.put('/api/list/:listId', jsonParser, function(req, res, next) {
+  debug('PUT: /api/list/:listId');
+
+  List.findByIdAndUpdate(req.params.listId, req.body, {new: true})
+    .then( list => res.json(list))
+    .catch(next);
+});
+
+
+//DELETE
+
+listRouter.delete('/api/list/:listId', function(req, res, next) {
+  debug('DELETE: /api/list/:listId');
+
+  List.findByIdAndRemove(req.params.listId)
+    .catch(next);
+});
 
