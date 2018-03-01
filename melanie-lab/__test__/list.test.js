1e2171d67908e25f36c7f5380b76d45cfe560b68
Melanie Cohen <melaniebcohen@gmail.com>
2018-02-28 18:55:17 -0800

    PUT and DELETE working

--- melanie-lab/__test__/list.test.js
+++ melanie-lab/__test__/list.test.js
@@ -51,18 +51,83 @@ describe('List Routes', function() {
           })
           .catch(done);
       });
-      afterEach( done => {
-        delete exampleList.timestamp;
-        if (this.tempList) {
-          List.remove({})
-            .then( () => done)
-            .catch(done);
-          return;
-        }
-        done();
-      });
+
+      // afterEach( done => {
+      //   delete exampleList.timestamp;
+      //   if (this.tempList) {
+      //     List.remove({})
+      //       .then( () => done())
+      //       .catch(done);
+      //     return;
+      //   }
+      //   done();
+      // });
+
       it('should return a list', done => {
         request.get(`${url}/api/list/${this.tempList._id}`)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(res.body.name).toEqual(exampleList.name);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('PUT: /api/list/:listId', function() {
+    describe('with a valid id & request body', function() {
+      beforeEach( done => {
+        exampleList.timestamp = new Date();
+        new List(exampleList).save()
+          .then( list => {
+            this.tempList = list;
+            done();
+          })
+          .catch(done);
+      });
+      
+      // afterEach( done => {
+      //   if (this.tempList) {
+      //     List.remove({})
+      //       .then( () => done() )
+      //       .catch(done);
+      //     return;
+      //   }
+      //   done();
+      // });
+
+      it.only('should update a list', done => {
+        exampleList.name = 'Updated list';
+        request.put(`${url}/api/list/${this.tempList._id}`)
+          .send(exampleList)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            console.log(res.body);
+            expect(res.body.name).toEqual('Updated list');
+            this.tempList = res.body;
+            done();
+          });
+      });
+    });
+  });
+
+  
+  describe('DELETE: /api/list/:listId', function() {
+    describe('with a valid body', function() {
+      beforeEach( done => {
+        exampleList.timestamp = new Date();
+        new List(exampleList).save()
+          .then( list => {
+            this.tempList = list;
+            done();
+          })
+          .catch(done);
+      });
+
+      it('should delete a list', done => {
+        request.delete(`${url}/api/list/${this.tempList._id}`)
           .end((err, res) => {
             if (err) return done(err);
             expect(res.status).toEqual(200);
--- melanie-lab/router/list-router.js
+++ melanie-lab/router/list-router.js
@@ -22,3 +22,20 @@ listRouter.post('/api/list', jsonParser, function(req, res, next) {
     .then( list => res.json(list))
     .catch(next);
 });
+
+listRouter.put('/api/list/:listId', jsonParser, function(req, res, next) {
+  debug('PUT: /api/list/:listId');
+
+  List.findByIdAndUpdate(req.params.listId, req.body, {new: true})
+    .then( list => res.json(list))
+    .catch(next);
+});
+
+
+listRouter.delete('/api/list/:listId', function(req, res, next) {
+  debug('DELETE: /api/list/:listId');
+
+  List.findByIdAndRemove(req.params.listId)
+    .then( list => res.json(list))
+    .catch(next);
+});
